b3c8c935f7965781bae6972fd811b00ade61799e
Seth-Race <sethrace@gmail.com>
2021-07-02 14:07:19 -0400

    More instruction

---
+++ FizzBuzzAppSol/FizzBuzzApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FizzBuzzApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FizzBuzz.");
+            for (var i = 1; i <= 30; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine("FIZZBUZZ");
+                } else if (i % 3 == 0 && i % 5 != 0) {
+                    Console.WriteLine("FIZZ");
+                } else if (i % 3 != 0 && i % 5 == 0) {
+                    Console.WriteLine("BUZZ");
+                } else {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Thank you for using the app. Goodbye.");
+        }
+    }
+}
---
+++ MyFirstCSharpConsoleAppSolution/MyFirstCSharpConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstCSharpConsoleApp
+{
+    class Program {
+        static void Main(string[] args) {
+            var hw = "Hello, world.";
+            Console.WriteLine(hw);
+            
+        }
+    }
+}
